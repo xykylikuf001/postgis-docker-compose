aa00291a55416d71433d6fa2366a6669bba5802c
Batyr Jumageldiyew <jb12345@inbox.ru>
2025-08-28 21:11:15 +0500

    fix initialize

--- src/db/initialize.sh
+++ src/db/initialize.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+set -e
 
-su - postgres -c "psql -t -c \"ALTER SYSTEM SET max_prepared_transactions = 100;\""
+
+# su - postgres -c "psql -t -c \"ALTER SYSTEM SET max_prepared_transactions = 100;\""
+# This script is run as 'postgres' user during init
+psql -U postgres -d postgres -c "ALTER SYSTEM SET max_prepared_transactions = 100;"
